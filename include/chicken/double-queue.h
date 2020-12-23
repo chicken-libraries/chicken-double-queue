f5551d6f1797c10233697928a8a0d60b84f6ca67
Michael <michaelbrockus@gmail.com>
2020-12-23 12:56:37 -0800

    give API for package

---
+++ include/chicken/double-queue.h
@@ -0,0 +1,50 @@
+//
+// file: double-queue.h
+// author: Michael Brockus
+// gmail: <michaelbrockus@gmail.com>
+//
+#ifndef CHICKEN_DEQUEUE_PACKAGE_H
+#define CHICKEN_DEQUEUE_PACKAGE_H
+
+//
+// Macros to control the visibility of functions provided by this package
+//
+#ifdef BUILDING_CHICKEN_DEQUEUE_PACKAGE
+#define CHICKEN_PUBLIC __attribute__((visibility("default")))
+#else
+#define CHICKEN_PUBLIC
+#endif
+
+//
+//   PUBLIC APPLICATION INTERFACE
+// --------------------------------
+//
+// Published package with provided public application interface for
+// use in the users application. Please note that we are internationally
+// targeting c23 standard.
+//
+
+typedef struct DeQueueNode DeQueueNode;
+typedef struct DeQueueOf DeQueueOf;
+
+//
+// Constructor and destructor functions
+//
+
+CHICKEN_PUBLIC DeQueueOf *chickenDeQueueCreate(void);
+CHICKEN_PUBLIC void chickenDeQueueErase(DeQueueOf **structureRef);
+
+//
+// Operations provided
+//
+
+CHICKEN_PUBLIC void chickenDeQueuePushStart(DeQueueOf *structurePtr, char *data);
+CHICKEN_PUBLIC void chickenDeQueuePushEnd(DeQueueOf *structurePtr, char *data);
+CHICKEN_PUBLIC char *chickenDeQueuePopStart(DeQueueOf *structurePtr);
+CHICKEN_PUBLIC char *chickenDeQueuePopEnd(DeQueueOf *structurePtr);
+CHICKEN_PUBLIC char *chickenDeQueuePeekStart(DeQueueOf *structurePtr);
+CHICKEN_PUBLIC char *chickenDeQueuePeekEnd(DeQueueOf *structurePtr);
+CHICKEN_PUBLIC unsigned int chickenDeQueueItsEmpty(DeQueueOf *structurePtr);
+CHICKEN_PUBLIC unsigned int chickenDeQueueNotEmpty(DeQueueOf *structurePtr);
+
+#endif // end of CHICKEN_PACKAGE_H
